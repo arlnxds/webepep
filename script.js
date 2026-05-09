0856350cc550237cee208c764287a699ed8cffb9
arlnxds <arlangg757@gmail.com>
2026-05-09 11:32:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-// ======================= script.js =======================
+const toggle = document.getElementById("themeToggle");
 
-// Klik link
-const links = document.querySelectorAll('.link-card');
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    console.log('Membuka:', link.textContent);
-  });
-});
-
-// Copy link
-const copyButtons = document.querySelectorAll('.copy-btn');
-copyButtons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const link = btn.getAttribute('data-link');
-    navigator.clipboard.writeText(link);
-    alert('Link berhasil disalin!');
-  });
-});
-
-// Dark mode toggle
-const toggle = document.getElementById('themeToggle');
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
 });
